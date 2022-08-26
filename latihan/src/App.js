df5a276c27c3159b6e7c78c5b6b56f91eccf2e26
master~ <s.btudz@gmail.com>
2022-08-26 09:57:32 +0800

    Membuar form selesai

--- latihan/src/App.js
+++ latihan/src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      firstname: "",
+      lastname: "",
+      name: "",
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    this.setState({
+      name: `${this.state.firstname} ${this.state.lastname}`,
+    });
+
+    this.state.firstname = "";
+    this.state.lastname = "";
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div className="p-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card">
+                <div className="card-header">Learn React Js</div>
+                <div className="card-body">
+                  <form onSubmit={this.submitHandler}>
+                    <div className="mb-4">
+                      <label htmlFor="firstname" className="form-label">
+                        Firstname
+                      </label>
+                      <input
+                        onChange={this.changeHandler}
+                        value={this.state.firstname}
+                        type="text"
+                        name="firstname"
+                        id="firstname"
+                        className="form-control"
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="lastname" className="form-label">
+                        Lastname
+                      </label>
+                      <input
+                        onChange={this.changeHandler}
+                        value={this.state.lastname}
+                        type="text"
+                        name="lastname"
+                        id="lastname"
+                        className="form-control"
+                      />
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">
+                      Show
+                    </button>
+                  </form>
+                </div>
+                <div className="card-footer">
+                  My Name is {name ? name : "..."}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
