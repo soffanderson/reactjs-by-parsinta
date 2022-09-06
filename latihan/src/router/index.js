fd5aace1a5cdd5ae0a0df8f212fdc54d133351f3
master~ <s.btudz@gmail.com>
2022-09-06 16:59:36 +0800

    Perbaikan

--- latihan/src/router/index.js
+++ latihan/src/router/index.js
@@ -4,21 +4,14 @@ import Home from "../views/Home";
 import About from "../views/About";
 import Contact from "../views/Contact";
 import NotFoundPage from "../views/errors/NotFoundPage";
-import Navbar from "../components/Navbar";
 
 function Router(props) {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
-      </Routes>
-      <Routes>
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Routes>
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Routes>
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
