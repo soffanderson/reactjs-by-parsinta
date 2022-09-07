ec4c4683a03fbe8a036f0abd921e81fa122bc937
soffanhdyt <s.btudz@gmail.com>
2022-09-07 21:55:57 +0800

    prev

--- latihan/src/App.js
+++ latihan/src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
 import Router from "./router";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFoundPage from "./views/errors/NotFoundPage";
 
 function App(props) {
-  return (
-    <div>
+  const NotFound = () => {
+    <>
+      <Route path="*" element={<NotFoundPage />} />
+    </>;
+  };
+
+  const DefaultContainer = () => {
+    <>
       <Navbar />
       <div className="py-4">
         <Router />
       </div>
-    </div>
+    </>;
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="*" component={NotFound} />
+        <Route component={DefaultContainer} />
+      </Routes>
+
+      {/* <Navbar />
+        <div className="py-4">
+          <Router />
+        </div> */}
+    </BrowserRouter>
   );
 }
 
